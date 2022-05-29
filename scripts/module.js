97df3ba78aa911538e0671b3dd7c1492631e416a
Streampunk <glad.car1474@fastmail.com>
2022-05-29 19:06:49 +0200

    use pre functions to set permission

--- scripts/module.js
+++ scripts/module.js
@@ -8,20 +8,29 @@ Hooks.once('ready', async function() {
 const _spRegex = /^\d+\s+/i;
 let _spAspectMarker = "📖".normalize();
 let _spAspectMarkerLen = _spAspectMarker.length
-function _spChatAspect(document, userId) {
+function _spChatAspect(document, data, userId) {
     if(!document.folder) {
         return;
     }
     if(!document.folder.data) {
         return;
     }
-    if(userId != game.user.data._id) {
+    let ddata = document.data;
+    let name = ddata.name;
+    let content = ddata.content;
+    if(data.name) {
+        name = data.name;
+    }
+    if(data.content) {
+        content = data.content;
+    }
+    if(ddata.name == name && ddata.content == content) {
         return;
     }
     let folder = document.folder.data.name.normalize();
     let info = folder.slice(0, _spAspectMarkerLen * -1).trim();
     info = info.replace(_spRegex, "");
-    let text = `<p><b>${info}: ${document.data.name}</b></p><p>${document.data.content}</p>`
+    let text = `<p><b>${info}: ${name}</b></p><p>${content}</p>`
     if(folder.endsWith(_spAspectMarker)) {
         ChatMessage.create({
             speaker: ChatMessage.getSpeaker({user: game.user}),
@@ -30,19 +39,18 @@ function _spChatAspect(document, userId) {
     }
 }
 
-Hooks.on("updateJournalEntry", async function(document, change, options, userId) {
-    document.update({permission: {default: 3}});
-    if(change.name == undefined && change.content == undefined) {
-        return;
+Hooks.on("preUpdateJournalEntry", async function(document, change, options, userId) {
+    if(change.permission) {
+        change.permission.default = 3;
+    } else {
+        change.permission = {default: 3};
     }
-    _spChatAspect(document, userId);
-});
-
-Hooks.on("createJournalEntry", async function(document, options, userId) {
-    document.data.permission.default = 3;
-    _spChatAspect(document, userId);
+    _spChatAspect(document, change, userId);
 });
 
 Hooks.on("renderJournalSheet", async function(app, html, data) {
     app.editors.content.button.onclick();
+    window.setTimeout(function() {
+        app.document.update({permission: {default: 3}});
+    }, 1000);
 });
