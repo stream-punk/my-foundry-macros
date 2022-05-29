59d38875ef5df6fd5d46d2c86bf24c5e76b93e4b
Streampunk <glad.car1474@fastmail.com>
2022-05-29 18:20:08 +0200

    also log creation of an aspect

--- scripts/module.js
+++ scripts/module.js
@@ -5,18 +5,22 @@ Hooks.once('init', async function() {
 Hooks.once('ready', async function() {
 });
 
+const _spRegex = /^\d+\s+/i;
 let _spAspectMarker = "📖".normalize();
 let _spAspectMarkerLen = _spAspectMarker.length
-Hooks.on("updateJournalEntry", async function(document, change, options, userId) {
-    document.update({permission: {default: 3}});
+function _spChatAspect(document, userId) {
+    if(!document.folder) {
+        return;
+    }
+    if(!document.folder.data) {
+        return;
+    }
     if(userId != game.user.data._id) {
         return;
     }
-    if(change.name == undefined && change.content == undefined) {
-        return;
-    }
     let folder = document.folder.data.name.normalize();
-    let info = folder.slice(2, _spAspectMarkerLen * -1).trim();
+    let info = folder.slice(0, _spAspectMarkerLen * -1).trim();
+    info = info.replace(_spRegex, "");
     let text = `<p><b>${info}: ${document.data.name}</b></p><p>${document.data.content}</p>`
     if(folder.endsWith(_spAspectMarker)) {
         ChatMessage.create({
@@ -24,6 +28,19 @@ Hooks.on("updateJournalEntry", async function(document, change, options, userId)
             content: text
         });
     }
+}
+
+Hooks.on("updateJournalEntry", async function(document, change, options, userId) {
+    document.update({permission: {default: 3}});
+    if(change.name == undefined && change.content == undefined) {
+        return;
+    }
+    _spChatAspect(document, userId);
+});
+
+Hooks.on("createJournalEntry", async function(document, options, userId) {
+    document.data.permission.default = 3;
+    _spChatAspect(document, userId);
 });
 
 Hooks.on("renderJournalSheet", async function(app, html, data) {
